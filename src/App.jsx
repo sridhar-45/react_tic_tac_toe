6db731146868a5196587de08c0109efe28198b09
Sridhar Goudu <sridhargoudu7@gmail.com>
2026-06-18 06:15:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Player from "./components/Player.jsx";
+
+export default function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="player 1" symbol="X" />
+          <Player initialName="player 2" symbol="O" />
+        </ol>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+
+export default function Player({ initialName, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+
+    function handleEditClick() {
+        // setIsEditing(!isEditing);
+        setIsEditing(editing => !editing);  //schedules a state update to  true
+    }
+
+    function handleChange(event){
+        setPlayerName(event.target.value);
+    }
+
+    let PlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        PlayerName = (
+            <input
+                type="text"
+                required
+                value={playerName}
+                onChange={handleChange}
+            />
+        );
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {PlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}> {isEditing ? "Save" : "Edit"}</button>
+        </li>
+    )
+}
