d90b3bda0d306c8ca0c4228165020667ea3e3671
Sridhar Goudu <sridhargoudu7@gmail.com>
2026-06-21 11:42:33 +0530

    Implement game board state management: initialize game board and handle square selection

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,23 +1,35 @@
-const intialGameBoard = [
+import { useState} from "react";
+const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
 export default function GameBoard(){
+        const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+        function handleSelectSquare(rowIndex, colIndex){
+            setGameBoard((prevGameBoard) => {   //prevGameBoard is a parameter containing the latest value of the gameBoard state
+                const updateGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; // Create a deep copy of the game board
+                updateGameBoard[rowIndex][colIndex] = "X"; // Example: set the selected square to "X"
+                return updateGameBoard;
+            });
+        }
         return (
             <ol id="game-board">
-            {intialGameBoard.map((row, rowIndex) => (
-                <li key={rowIndex}>
-                    <ol>
-                        {row.map((playerSymbol, colIndex) => (
-                            <li key={colIndex}>
-                                <button>{playerSymbol}</button>
-                            </li>
-                        ))}
-                    </ol>
-                </li>
-            ))}
+                {gameBoard.map((row, rowIndex) => (
+                    <li key={rowIndex}>
+                        <ol>
+                            {row.map((playerSymbol, colIndex) => (
+                                <li key={colIndex}>
+                                    <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                                        {playerSymbol}
+                                    </button>
+                                </li>
+                            ))}
+                        </ol>
+                    </li>
+                ))}
             </ol>
         )
 };
