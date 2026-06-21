dbffad567cd6fe77d2298c3745a76127a5593408
Sridhar Goudu <sridhargoudu7@gmail.com>
2026-06-21 23:07:04 +0530

    Fix Log component to correctly display player selection in move history

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -4,7 +4,7 @@ export default function Log( { turns }) {
             {turns.map(turn => (
                 <li key={`${turn.square.row}${turn.square.col}`}>
                     {console.log(`Player ${turn.player} selected ${turn.square.row}, ${turn.square.col}`)}
-                    {turn.square} selected {turn.square.row}, {turn.square.col}
+                    {turn.player} selected {turn.square.row}, {turn.square.col}
                 </li>
             ))}
         </ol>
